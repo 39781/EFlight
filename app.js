a5389012cb0a93a47afb2231f409d8f0021d1271
B Hari prasad <39781@hexaware.com>
2018-01-22 11:47:16 +0530

    Eflight api

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ var bodyParser = require('body-parser');
 var routes = require('./routes');
 var app = express();
 
-
+var port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: false })) 
 app.use(bodyParser.json());
 app.use(routes);
 
-var server = app.listen();
+var server = app.listen(port,function(){
+	console.log("Application started listening port "+port);
+	
+});
